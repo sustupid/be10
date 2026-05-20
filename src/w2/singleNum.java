b916b2e859a8731b1a408d774330af0d0dcbef18
vit <hauucool@gmail.com>
2026-05-20 19:30:27 +0930

    vcl

--- src/w2/singleNum.java
+++ src/w2/singleNum.java
@@ -3,12 +3,14 @@ package w2;
 public class singleNum {
     public static int singleNumber(int[] nums){
 
-        int num = nums[0];
+        int count = 0;
         for (int i = 0; i < nums.length - 1; i++){
-                if (nums[i] == 1) return 1;
-                else if (num != nums[i]) return num;
+                if (nums[i] == nums [i+1])
+                    count++;
+                else return nums[i];
             }
-            return 0;
+        if (count >= 2) return 0;
+
         }
 
     public static void main(String[] args){
